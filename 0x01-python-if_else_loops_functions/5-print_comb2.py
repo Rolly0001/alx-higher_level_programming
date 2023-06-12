b308285dc33f8849a2f64982d7f8a2b76a984355
Rolly0001 <rolandkanu91@gmail.com>
2023-06-12 07:20:39 -0700

    permutattion-like

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    print("{}, ".format(numbers),end="")
+for i in range(100):
+    if i == 99:
+        print(i)
+    else:
+        print("{}".format('0' + str(i) if i < 10 else i), end=", ")
