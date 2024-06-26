d35d0f788cdc7c455d36172b70ce1e992bf96294
Rolly0001 <rolandkanu91@gmail.com>
2024-06-26 08:32:15 -0700

    j

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,15 +1,12 @@
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    printed_count = 0
     try:
         for i in range(x):
             print(my_list[i], end="")
-            count += 1
+            printed_count += 1
     except IndexError:
-        pass
-    finally:
-        print("")  # Ensure a new line is printed at the end
-    return count
-
-# Example usage:
-my_list = [1, 2, 3, "a", "b", "c"]
-print("Number of elements printed:", safe_print_list(my_list, 10))
+        pass  # If index i is out of range, it will exit the loop
+    
+    print()  # Print newline after all elements
+    
+    return printed_count
