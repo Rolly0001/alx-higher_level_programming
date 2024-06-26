5e268606c8f355a1e2fa4740d5a37a13d9be5d66
Rolly0001 <rolandkanu91@gmail.com>
2024-06-26 08:40:55 -0700

    h

--- 0-safe_print_list.py
+++ 0-safe_print_list.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    printed_count = 0
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-            printed_count += 1
-    except IndexError:
-        pass  # If index i is out of range, it will exit the loop
-    
-    print()  # Print newline after all elements
-    
-    return printed_count
+    """Print x elememts of a list.
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
